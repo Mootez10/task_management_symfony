7d97d597fdbb43136cb867a1dbf1fa026d75fdeb
Mootez <mootezaouinti1@gmail.com>
2024-12-12 01:06:23 +0100

    include sidebar/navbar and footer

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
